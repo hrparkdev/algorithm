e17ba2032052c9640e6e231c0dd914a2ddff25f0
hrparkdev <hrparkdev@gmail.com>
2022-09-07 23:45:58 +0900

    1000

---
+++ boj/src/class1/Num1000.java
@@ -0,0 +1,20 @@
+package class1;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Num1000 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        bw.write(Integer.toString(a + b));
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
