a29a88723ab48f1c04176244ae63de01056f81ba
hrparkdev <hrparkdev@gmail.com>
2022-09-08 23:27:26 +0900

    1008

---
+++ boj/src/class1/Num1008.java
@@ -0,0 +1,20 @@
+package class1;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Num1008 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Double a = Double.parseDouble(st.nextToken());
+        Double b = Double.parseDouble(st.nextToken());
+        bw.write(Double.toString(a / b));
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
