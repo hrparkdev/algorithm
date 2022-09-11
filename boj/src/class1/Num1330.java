31f79e2280a834c71d48ca6c73c67147d069d3fd
hrparkdev <hrparkdev@gmail.com>
2022-09-11 17:49:58 +0900

    1330

---
+++ boj/src/class1/Num1330.java
@@ -0,0 +1,28 @@
+package class1;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Num1330 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        if (a > b) {
+            bw.write(">");
+        } else if (a < b) {
+            bw.write("<");
+        } else {
+            bw.write("==");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
