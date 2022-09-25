0a990b5d7a66bbbcbd62e483358e86522ceb9d92
hrparkdev <hrparkdev@gmail.com>
2022-09-25 23:36:04 +0900

    2438

---
+++ boj/src/class1/Num2438.java
@@ -0,0 +1,27 @@
+package class1;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Num2438 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int num = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+
+    }
+}
