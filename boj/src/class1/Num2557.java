52d9af3e0897f647b30b351a779982524e6aebee
hrparkdev <hrparkdev@gmail.com>
2022-10-05 09:35:42 +0900

    2557

---
+++ boj/src/class1/Num2557.java
@@ -0,0 +1,17 @@
+package class1;
+
+import java.io.*;
+
+public class Num2557 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("Hello World!");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
