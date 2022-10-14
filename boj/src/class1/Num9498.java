cdce4c142c76ddceb46d7c82f60f659eba68490c
hrparkdev <hrparkdev@gmail.com>
2022-10-14 11:33:51 +0900

    9498

---
+++ boj/src/class1/Num9498.java
@@ -0,0 +1,30 @@
+package class1;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Num9498 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int score = Integer.parseInt(st.nextToken());
+
+        if (score >= 90) {
+            bw.write("A");
+        } else if (score >= 80) {
+            bw.write("B");
+        } else if (score >= 70) {
+            bw.write("C");
+        } else if (score >= 60) {
+            bw.write("D");
+        } else {
+            bw.write("F");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
