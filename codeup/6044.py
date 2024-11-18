77caba12e705e2ce6fdf228a57dd8eae1fbc54b8
hrparkdev <hrparkdev@gmail.com>
2024-11-18 20:49:30 +0900

    feat: Solve CodeUp 6044번 문제

---
+++ codeup/6044.py
@@ -0,0 +1,15 @@
+n1, n2 = map(int, input().split())
+
+sum_result = n1 + n2
+sub_result= n1 - n2
+mul_result = n1 * n2
+quot_result = n1 // n2
+mod_result = n1 % n2
+div_result = n1 / n2
+
+print(sum_result)
+print(sub_result)
+print(mul_result)
+print(quot_result)
+print(mod_result)
+print(format(div_result, ".2f"))
